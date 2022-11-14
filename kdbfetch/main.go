afe6666b59a625ffdf8b66d54283b99568fb0e91
Nakaya <eniehack@outlook.jp>
2022-11-15 03:05:14 +0900

    feature: fetch a specific syllabus from kdb

---
+++ kdbfetch/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	"net/http"
+	"io"
+)
+const prefix = "https://kdb.tsukuba.ac.jp/syllabi/2022/%s/jpn"
+
+func main() {
+	app := &cli.App{
+		Name: "kdbfetch",
+		Usage: "fetch a specific subject",
+		Action: fetch,
+		UsageText: "kdbfetch [global options] [subject id]",
+	}
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+}
+
+func fetch(c *cli.Context) error {
+	number := c.Args().Get(0)
+
+	client := new(http.Client)
+	req, _ := http.NewRequest(
+		"GET",
+		fmt.Sprintf(prefix, number),
+		nil,
+	)
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error: status: %d\n", resp.StatusCode)
+	}
+	io.Copy(os.Stdout, resp.Body)
+
+	return nil
+}
