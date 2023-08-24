2546df25ef4ea05f69ce4bf327835574cf57f6dd
Nakaya <eniehack@outlook.jp>
2023-08-25 05:38:12 +0900

    feat: user-agent

--- kdbfetch/main.go
+++ kdbfetch/main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/urfave/cli/v2"
-	"net/http"
 	"io"
+	"net/http"
+
+	"github.com/urfave/cli/v2"
 )
+
+const USER_AGENT = "kdb2-crawler (+https://github.com/until-tsukuba/kdb2-crawler)"
 const prefix = "https://kdb.tsukuba.ac.jp/syllabi/2023/%s/jpn"
 
 func main() {
 	app := &cli.App{
-		Name: "kdbfetch",
-		Usage: "fetch a specific subject",
-		Action: fetch,
+		Name:      "kdbfetch",
+		Usage:     "fetch a specific subject",
+		Action:    fetch,
 		UsageText: "kdbfetch [global options] [subject id]",
 	}
 	if err := app.Run(os.Args); err != nil {
@@ -31,6 +34,7 @@ func fetch(c *cli.Context) error {
 		fmt.Sprintf(prefix, number),
 		nil,
 	)
+	req.Header.Set("User-Agent", USER_AGENT)
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("error: status: %d\n", resp.StatusCode)
